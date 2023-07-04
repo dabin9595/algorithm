c361cd6f7bc2da8e395fb66294d7cfd40fee7493
다빈 <107475889+dabin9595@users.noreply.github.com>
2023-07-04 23:37:14 +0900

    proframmers

---
+++ programmers/230704/230704.js
@@ -0,0 +1,11 @@
+주사위 게임 1
+
+const solution = (a, b) => {
+    if(a % 2 !== 0 && b % 2 !== 0){
+        return (a*a) + (b*b)
+    }else if (a % 2 !== 0 || b % 2 !== 0){
+        return 2*(a+b)
+    }else {
+        return Math.abs(a-b)
+    }
+}
